506501e25c5d020ea2c266b6b5ea82b554301902
J <jg688@njit.edu>
2018-10-04 19:21:56 -0400

    Feature : Created modular structure following SOLID

--- src/index.php
+++ src/index.php
@@ -5,4 +5,38 @@
  * Date: 10/4/18
  * Time: 9:49 AM
  */
-echo 'test mini project';
+main::start();
+Class main{
+    static public function start(){
+        $records = csv ::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+class csv{
+    static public function getRecords(){
+
+        $records = 'testing functions';
+        return $records;
+        
+    }
+}
+
+class html{
+    static public function generateTable($records){
+
+        $table = $records;
+        return $table;
+
+    }
+}
+
+class system{
+
+    static public function printPage($page){
+
+        echo $page;
+
+    }
+
+}
